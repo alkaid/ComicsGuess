653a4bca1047d3d355f4c967acbb8fcd312f8b38
xiamao03 <xiamao_46@163.com>
2013-09-25 10:05:32 +0800

    增加 主菜单界面 按键触发跳转

--- Engineering/CD/Client/ComicsGuess/src/com/coodroid/comicsguess/view/fragments/MainMenuFragment.java
+++ Engineering/CD/Client/ComicsGuess/src/com/coodroid/comicsguess/view/fragments/MainMenuFragment.java
@@ -3,10 +3,13 @@ package com.coodroid.comicsguess.view.fragments;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.alkaid.base.extern.baseView.BaseFragment;
 import com.coodroid.comicsguess.R;
+import com.coodroid.comicsguess.view.FragmentSwitchHelper;
 
 /**
  * 主菜单fragment
@@ -14,6 +17,10 @@ import com.coodroid.comicsguess.R;
  *
  */
 public class MainMenuFragment extends BaseFragment{
+	
+	private Button mainLevelBtn;
+	private Button mainChallengeBtn;
+	private Button mainShareBtn;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -24,12 +31,44 @@ public class MainMenuFragment extends BaseFragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.main_menu_layout, null);
+		mainLevelBtn = (Button) view.findViewById(R.id.main_level_btn);
+		mainChallengeBtn = (Button) view.findViewById(R.id.main_challenge_btn);
+		mainShareBtn = (Button) view.findViewById(R.id.main_share_btn);
+		setOnBtnListener();
 		return view;
 	}
 	
 	@Override
 	public void onPause() {
 		super.onPause();
+	}
+	
+	//各个按键的监听
+	private void setOnBtnListener(){
+		if(mainLevelBtn!=null){
+			mainLevelBtn.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					FragmentSwitchHelper.getInstance().skipFragment(FragmentSwitchHelper.LEVEL_SELECT_FRAGMENT);
+				}
+			});
+		}
+		if(mainChallengeBtn!=null){
+			mainChallengeBtn.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					
+				}
+			});
+		}
+		if(mainShareBtn!=null){
+			mainShareBtn.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					
+				}
+			});
+		}
 	}
 
 }
