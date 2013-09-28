81bc96a995cd16b79c608137e104f48243ad0ead
xiamao03 <xiamao_46@163.com>
2013-09-28 21:02:37 +0800

    建地图编辑器工程

---
+++ Engineering/CD/Tools/CGMapBuilder/src/com/coodroid/cgmapbuilder/CGMapBuilderMain.java
@@ -0,0 +1,12 @@
+package com.coodroid.cgmapbuilder;
+
+import com.coodroid.cgmapbuilder.view.CGMapBuilderFrame;
+
+/**
+ */
+public class CGMapBuilderMain {
+
+	public static void main(String[] args) {
+		CGMapBuilderFrame frame = new CGMapBuilderFrame();
+	}
+}
